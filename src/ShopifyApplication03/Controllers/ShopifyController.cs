a051461ce9c9d0070788de13ac4010eca7e1f19b
anhlt100 <anhlt100@fpt.com.vn>
2022-04-25 21:58:23 +0700

    init shopify app

---
+++ src/ShopifyApplication03/Controllers/ShopifyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopifyApplication03.Controllers
+{
+    public class ShopifyController : Controller
+    {
+        public IActionResult Install()
+        {
+            return View();
+        }
+    }
+}
